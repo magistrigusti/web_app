6d9453bc326adfe3c6871fbe5509c84b697a71cf
Magistry <Magistrigusti@yandex.ru>
2023-08-25 17:19:57 +0300

    0.04ba2

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
       <Button onClick={onClose}>Close it</Button>
 
       <span className='username'>
-        {user?.first_name}
+        {user?.username}
       </span>
 
     </div>
