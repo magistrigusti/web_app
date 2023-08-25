b52502bfd091559628a582b59ca12f0d26387592
Magistry <Magistrigusti@yandex.ru>
2023-08-25 09:07:41 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
 
+const telegramWebApp = window.Telegram.WebApp;
+
+
 function App() {
+
+  const onClose = () => {
+    telegramWebApp.close();
+  };
+
+  useEffect(() => {
+    telegramWebApp.ready();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      App
+
+      <button onClick={onClose}>Close</button>
     </div>
   );
 }
