c30a2696e156631e46446bcb82b5d8549805d5b7
Magistry <Magistrigusti@yandex.ru>
2023-08-25 14:04:47 +0300

    0.02

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-
+import React, { useEffect } from 'react';
 import './App.css';
 
+const telWebApp = window.Telegram.WebApp;
 
 function App() {
+
+  useEffect(() => {
+    telWebApp.ready();
+  }, []);
 
   return (
     <div className="App">
