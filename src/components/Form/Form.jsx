8c87c00d495097eaf8432488ef8ab7a349ddd456
Magistry <Magistrigusti@yandex.ru>
2023-08-26 05:42:47 +0300

    0.05a

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,15 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Form.css';
 
 const Form = () => {
+  const [country, setCountry] = useState('');
+  const [city, setCity] = useState('');
+  const [subject, setSubject] = useState('physical');
+
+  const onChangeCountry = (event) => {
+    setCountry(event.target.value);
+  };
+
+  const onChangeCity = (event) => {
+    setCity(event.target.value);
+  };
+
+  const onChangeSubject = (event) => {
+    setSubject(event.target.value);
+  }
+
   return (
     <div className="form">
       <h3>Enter your details</h3>
     
-      <input className="input" type="text" placeholder="Country" />
-      <input classname="input" type="text" placeholder="Street" />
+      <input className="input" 
+          type="text" 
+          placeholder="Country" 
+          value={country}
+          onChange={onChangeCountry}
+      />
+      <input classname="input" 
+          type="text" 
+          placeholder="City"
+          value={city}
+          onChange={onChangeCity}
+      />
 
-      <select className="select">
+      <select className="select" onChange={onChangeSubject}>
         <option value={'physical'}>Phisical</option>
         <option value={'legal'}>Legal</option>
       </select>
